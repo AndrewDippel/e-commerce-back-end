7203f07e1c0ea657237c0f3d54cef56532e5905f
Andrew Dippel <a.t.dippel@gmail.com>
2022-10-28 20:09:47 +1100

    define columns for category,product,productTag

--- models/index.js
+++ models/index.js
@@ -5,6 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsToMany(Category, {
+  through: {
+    model: Trip,
+    unique: false
+  },
+  as: 'planned_trips'
+});
 
 // Categories have many Products
 
